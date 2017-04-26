caa7584bf08cf8fdeb930c93f5b7486698382ef6
Charles Tang <chuck.tang98@gmail.com>
2017-04-25 17:51:50 -0700

    trie questions

--- Java_Solutions/src/Main.java
+++ Java_Solutions/src/Main.java
@@ -1,32 +1,12 @@
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static BufferedReader in;
-    static PrintWriter out;
-
     public static void main(String[] args) throws IOException {
-        in = new BufferedReader(new FileReader("input.in"));
-        //in = new BufferedReader(new InputStreamReader(System.in));
-        //out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-        out = new PrintWriter(new BufferedWriter(new FileWriter("output.out")));
-        StringTokenizer st = new StringTokenizer(in.readLine());
-        int testcases = Integer.parseInt(st.nextToken());
-        for (int t = 1; t <= testcases; t++) {
-            TaskB solver = new TaskB();
-            solver.solve(t);
-        }
-        out.close();
-        System.exit(0);
-    }
+        //Scanner in = new Scanner(new FileReader("input.in"));
+        Scanner in = new Scanner(System.in);
 
-    static class TaskB {
-        final int MAXN = 110;
-        int N;
-
-        public void solve(int t) throws IOException {
-            Scanner in = new Scanner(System.in);
-            //System.out.println();
-        }
     }
 }
+
